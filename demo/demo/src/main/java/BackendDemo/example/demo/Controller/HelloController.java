3a2ecb0142ea53ea8d6b3dad5ce08ba824083dd7
deveshdubey18 <314devesh11scic@gmail.com>
2026-05-10 17:26:24 +0530

    initial commit

---
+++ demo/demo/src/main/java/BackendDemo/example/demo/Controller/HelloController.java
@@ -0,0 +1,13 @@
+package BackendDemo.example.demo.Controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello(){
+        return "Spring boot Application is running";
+    }
+}
